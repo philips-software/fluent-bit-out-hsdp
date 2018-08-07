d3f422216c78cacd700d4784e432fe90234c18cc
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2018-08-08 00:24:28 +0200

    Prepare for development

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM golang:1.10 AS builder
+
+WORKDIR /go/src/github.com/loafoe/fluent-bit-go-hsdp-output/
+
+COPY .git Makefile Gopkg.* *.go /go/src/github.com/loafoe/fluent-bit-go-hsdp-output/
+RUN go get -u github.com/golang/dep/cmd/dep \
+ && make dep all
+
+FROM fluent/fluent-bit:0.13.6
+
+COPY --from=builder /go/src/github.com/loafoe/fluent-bit-go-hsdp-output/out_hsdp.so /fluent-bit/bin/
+COPY *.conf /fluent-bit/etc/
+COPY start.sh /start.sh
+
+CMD ["/start.sh"]
--- out_hsdp.go
+++ out_hsdp.go
@@ -1,27 +1,40 @@
 package main
 
 import (
+	"C"
+	"fmt"
 	"unsafe"
 
-	"C"
 	"github.com/fluent/fluent-bit-go/output"
 	_ "github.com/philips-software/go-hsdp-api/logging"
 )
 
+//export FLBPluginRegister
+func FLBPluginRegister(ctx unsafe.Pointer) int {
+	return output.FLBPluginRegister(ctx, "hsdp", "HSDP logging output plugin")
+}
+
 //export FLBPluginInit
 func FLBPluginInit(ctx unsafe.Pointer) int {
-	return output.FLBPluginRegister(ctx, "hsdp", "HSDP logging")
+	return output.FLB_OK
 }
 
 //export FLBPluginFlush
 func FLBPluginFlush(data unsafe.Pointer, length C.int, tag *C.char) int {
 	// do something with the data
-	return 0
+
+	// Return options:
+	//
+	// output.FLB_OK    = data have been processed.
+	// output.FLB_ERROR = unrecoverable error, do not try this again.
+	// output.FLB_RETRY = retry to flush later.
+	fmt.Printf("Flushing..\n")
+	return output.FLB_OK
 }
 
 //export FLBPluginExit
 func FLBPluginExit() int {
-	return 0
+	return output.FLB_OK
 }
 
 func main() {
